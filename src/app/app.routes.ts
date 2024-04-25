8b8501bf2cec53c2820c305a306e74dc5451f9df
Santiago <santib.2001@hotmail.com>
2024-04-24 22:55:10 -0500

    feat: Added basic structure for guards app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', title: 'Home', component: HomeComponent },
+  { path: 'admin-panel', title: 'Panel Admin', component: AdminPanelComponent },
+];
